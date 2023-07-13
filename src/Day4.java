2e03898ab80a15afe71e1263f49d00c70a573d71
Saurabh Bodke <saurabhbodke012@gmail.com>
2023-07-13 11:15:28 -0500

    Day4 , Problem 2- Contiguous Array

--- src/Day4.java
+++ src/Day4.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class Day4 {
@@ -29,7 +30,34 @@ public class Day4 {
     }
     
 
-    //
-    
+    //Contiguous Array
+     public int findMaxLength(int[] nums) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0,-1);
+        int arrSum = 0;
+        int result =0;
+
+
+        for (int i=0;i<nums.length; i++){
+
+            if(nums[i] == 0){
+                arrSum--;
+            }else{
+                arrSum++;
+            }
+
+
+            if( map.containsKey(arrSum)){
+                result = Math.max(result, i - map.get(arrSum));
+            }else{
+                map.put(arrSum, i);
+            }
+
+        }
+
+        return result;
+        
+    }
 
 }
