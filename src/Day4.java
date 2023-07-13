0b086d5d1d72256908fe0f66049789c63c09d1e1
Saurabh Bodke <saurabhbodke012@gmail.com>
2023-07-13 11:35:35 -0500

    Day4 , Problem 3- Subarray Sum Equals K

--- src/Day4.java
+++ src/Day4.java
@@ -60,4 +60,31 @@ public class Day4 {
         
     }
 
+
+    //Subarray Sum Equals K
+    public int subarraySum(int[] nums, int k) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0,1);
+
+        int rSum = 0;
+        int res = 0;
+
+        for(int i =0; i<nums.length; i++){
+
+            rSum += nums[i];
+
+            if( map.containsKey(rSum -k)){
+                res += map.get(rSum-k);
+            }
+            if ( !(map.containsKey(rSum))){
+                map.put(rSum, 0);
+            }
+            map.put(rSum, map.get(rSum)+1);
+
+        }
+        
+        return res;
+    }
+
 }
