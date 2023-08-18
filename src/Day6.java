90a9af92de1cb715d2ab5c8a4d280ad609b89c12
Saurabh Bodke <saurabhbodke012@gmail.com>
2023-08-18 16:34:30 -0500

    Day7 , Problem 2-Minimum Falling Path Sum

--- src/Day6.java
+++ src/Day6.java
@@ -50,4 +50,42 @@ public class Day6 {
         
     }
 
+    // Minimum Falling Path Sum
+    public int minFallingPathSum(int[][] matrix) {
+        
+        if (matrix == null || matrix.length == 0){
+            return 0;
+        }
+
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int[][] dp = new int[n][m];
+
+        for( int i=0; i<n; i++){
+            dp[n-1][i] = matrix[n-1][i];
+        } 
+
+        for( int i=n-2; i>=0; i--){
+            for( int j=0; j<m; j++){
+                if ( j ==0 ){
+                    dp[i][j] = matrix[i][j] + Math.min(dp[i+1][j], dp[i+1][j+1]);
+                }else if(j == m-1){
+                    dp[i][j] = matrix[i][j] + Math.min(dp[i+1][j], dp[i+1][j-1]);
+                }else{
+                    dp[i][j] = matrix[i][j] + Math.min(dp[i+1][j], Math.min(dp[i+1][j+1], dp[i+1][j-1]));
+                }
+            }
+        }
+
+        int res =Integer.MAX_VALUE ;
+        for(int i=0; i<n; i++){
+            int check = dp[0][i];
+            res = Math.min(check, res);
+        }
+
+        return res;
+
+    }
+
 }
