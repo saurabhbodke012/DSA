2b5df46cc12ea1c56e29f34a44314d06cebbe71d
Saurabh Bodke <saurabhbodke012@gmail.com>
2023-08-19 21:38:40 -0500

    Day8 , Problem 2- House Robber II

--- src/Day8.java
+++ src/Day8.java
@@ -27,4 +27,47 @@ public class Day8 {
         return dp[lastDay];
     }
     
+
+    //House Robber II
+    public int rob(int[] nums) {
+        
+        if(nums == null || nums.length ==0){
+            return 0;
+        }
+
+        if( nums.length == 1){
+            return nums[0];
+        }
+
+        if( nums.length == 2){
+            return Math.max(nums[0], nums[1]);
+        }
+
+        int[] arr1 = new int[nums.length-1];
+        int[] arr2 = new int[nums.length-1];
+
+        for(int i=0; i<arr1.length; i++){
+            arr1[i] = nums[i];
+            arr2[i] = nums[i+1];
+        }
+
+        int rob1 = helper(arr1);
+        int rob2 = helper(arr2);
+
+        return Math.max(rob1, rob2);
+
+    }
+
+    public static int helper(int[] nums){
+        int[] dp = new int[nums.length];
+
+        dp[0] = nums[0];
+        dp[1] = Math.max(dp[0], nums[1]);
+
+        for(int i=2; i<dp.length; i++){
+            dp[i] = Math.max( dp[i-1], nums[i]+ dp[i-2]);
+        }
+
+        return dp[nums.length-1];
+    }
 }
