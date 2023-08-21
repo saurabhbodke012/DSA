68e6ec1d7ce8ee77b8bd8df43555964377370a5b
Saurabh Bodke <saurabhbodke012@gmail.com>
2023-08-21 16:29:23 -0500

    Grind 75, Week1 - Valid Parentheses

--- src/Grind75/Week1.java
+++ src/Grind75/Week1.java
@@ -1,6 +1,7 @@
 package Grind75;
 
 import java.util.HashMap;
+import java.util.Stack;
 
 public class Week1 {
 
@@ -23,5 +24,25 @@ public class Week1 {
         return null;
     }
     
-    
+    // Valid Parentheses
+     public boolean isValid(String s) {
+
+     Stack<Character> stack = new Stack<Character>();
+
+    for( char c : s.toCharArray()){
+        if( c == '('){
+            stack.push(')');
+        }else if( c == '['){
+            stack.push(']');
+        }else if( c == '{'){
+            stack.push('}');
+        }else if(stack.isEmpty() || stack.pop() != c){
+            return false;
+        }
+    }
+
+    return stack.isEmpty();
+
+    }
+
 }
