4e7dcc046842bf1c394354544f35e6b94a9f0b34
Saurabh Bodke <saurabhbodke012@gmail.com>
2023-09-05 14:12:43 -0400

    Day12, Problem 1- Remove Duplicates from sorted array II

--- src/Day12.java
+++ src/Day12.java
@@ -1,3 +1,31 @@
 public class Day12 {
+
+    // Remove Duplicates from sorted array II
+    public int removeDuplicates(int[] nums) {
+
+        if ( nums == null || nums.length == 0){
+            return 0;
+        }
+
+        int fast = 1;
+        int slow = 1;
+        int count = 1;
+
+        for( fast = 1; fast< nums.length; fast++){
+
+            if( nums[fast] == nums[fast-1]){
+                count++;
+            }else{
+                count = 1;
+            }
+
+            if( count <=2){
+                nums[slow] = nums[fast];
+                slow++;
+            }
+
+        }
+        return slow;
+    }
     
 }
