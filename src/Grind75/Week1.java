8acd7aa5a1d06e09a237bbddb116bd6b1d928235
Saurabh Bodke <saurabhbodke012@gmail.com>
2023-09-06 16:21:21 -0400

    Week 1: Best time to buy and Sell

--- src/Grind75/Week1.java
+++ src/Grind75/Week1.java
@@ -45,4 +45,21 @@ public class Week1 {
 
     }
 
+    // Best Time to Buy and Sell
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+
+        for( int i =0; i< prices.length; i++){
+
+            min = Math.min(prices[i], min);
+
+            if(prices[i] - min > max){
+                max = prices[i] - min;
+            }
+        }
+
+        return max;
+    }
+
 }
