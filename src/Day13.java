326b866e51c0283369fd3d81a5cd906dafb5b9c4
Saurabh Bodke <saurabhbodke012@gmail.com>
2023-09-06 15:28:36 -0400

    Day 13- Problem 2:3Sum

--- src/Day13.java
+++ src/Day13.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Day13 {
     
     // Contains most water 
@@ -22,5 +26,49 @@ public class Day13 {
         }
 
         return result;
+    }
+
+    // 3Sum 
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+
+        int n = nums.length;
+        Arrays.sort(nums);
+        
+        for ( int i =0; i<n-2; i++ ){
+
+            if( i !=0 && nums[i] == nums[i-1]){
+                continue;
+            }
+
+            int low = i+1;
+            int high = n-1;
+
+            while ( low< high){
+                int sum = nums[i] + nums[low] + nums[high];
+
+                if ( sum == 0){
+                    List<Integer> res = Arrays.asList( nums[i], nums[low], nums[high]);
+                    list.add(res);
+                    low++;
+                    high--;
+                    
+                    while ( low<high && nums[low] == nums[low-1]){
+                        continue;
+                    }
+
+                    while( low<high && nums[high] == nums[high+1]){
+                        continue;
+                    }
+                }else if ( sum>0){
+                    high --;
+                }else{
+                    low++;
+                }
+
+            }
+        }
+
+        return list;
     }
 }
