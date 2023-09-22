3a1e3fa32af1a4eaef5149a7dd6237882bbdc171
Saurabh Bodke <saurabhbodke012@gmail.com>
2023-09-22 10:51:14 -0400

    Porblem 2- Day 15

--- src/Day15.java
+++ src/Day15.java
@@ -37,4 +37,38 @@ public class Day15 {
 
         return root;
     }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    // Remove nth Node from LinkedList
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        
+        if( head == null){
+            return null;
+        }
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+        int count =0;
+
+        while( count <=n){
+            fast = fast.next;
+            count++;
+        }
+
+        while(fast != null){
+            slow = slow.next;
+            fast= fast.next;
+        }
+
+        slow.next = slow.next.next; 
+        return dummy.next;
+    }
 }
